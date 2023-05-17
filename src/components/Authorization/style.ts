d2effe461d4ef64adbafd28713e1872b1b195466
Yura Chekhov <ychekhov@bk.ru>
2023-05-18 02:12:52 +0300

    refactoring

---
+++ src/components/Authorization/style.ts
@@ -0,0 +1,11 @@
+import { styled } from "styled-components";
+
+
+export const Form = styled('form')({
+    display: 'flex',
+    flexDirection: 'column',
+    width: '300px',
+    height: '200px',
+    margin: '0 auto',
+    justifyContent: 'space-between'
+})
