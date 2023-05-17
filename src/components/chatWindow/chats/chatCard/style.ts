66d9b390e899d2f3a7122ddde1a63eb77af5ce4d
Ivan Ponosov <ponosov.v49@gmail.com>
2023-05-17 17:12:43 +0500

    Добавил чаты в сайдбар

---
+++ src/components/chatWindow/chats/chatCard/style.ts
@@ -0,0 +1,19 @@
+import { styled } from "@mui/material";
+
+export const ChatCardWrapper = styled("div")({
+	display: "flex",
+	alignItems: "center",
+	"&:hover": {
+		backgroundColor: "#f0f2f5",
+	},
+});
+
+export const UserDataWrapper = styled("div")({
+	display: "flex",
+	flexDirection: "column",
+	fontSize: "12px",
+	borderBottom: "1px solid #f0f2f5",
+	"& :nth-child(1n)": {
+		margin: "5px",
+	},
+});
